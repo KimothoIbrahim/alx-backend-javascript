ec41eed2c1f845f4f25fca39f1f21d85fc8171f8
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-08-07 12:55:43 +0300

    Handling promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 function handleResponseFromAPI (promise) {
   promise.then((response) => {
     console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
+    return { status: 200, body: response };
   },
   (error) => { return Error(); });
 }
