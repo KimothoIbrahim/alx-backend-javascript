7e152424d5a845c4286e5d8fad700344578e2245
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-08-09 10:29:11 +0300

    All promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,8 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-function handleProfileSignup () {
+export function handleProfileSignup () {
   Promise.all([uploadPhoto(), createUser()]).then((res) => {
     const [{ body }, { firstName, lastName }] = res;
     console.log(`${body} ${firstName} ${lastName}`);
   }).catch(() => console.log('Signup system offline'));
 }
-
-module.exports = handleProfileSignup;
