b437e73f5973265e28c4a11cca42edffc1d84843
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-08-09 21:42:57 +0300

    Resolving many promises at a go

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,9 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((res) => {
-    const [{ body }, { firstName, lastName }] = res;
-    console.log(`${body} ${firstName} ${lastName}`);
-  }).catch(() => console.log('Signup system offline'));
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  }).catch((err) => console.log('Signup system offline'));
 }
 
 module.exports = handleProfileSignup;
