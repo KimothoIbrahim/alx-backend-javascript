aeb2dd994ff29ec9d2a80457b1880320f86b8694
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-08-16 22:40:51 +0300

    Map a map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,9 @@
 export default function updateUniqueItems(map) {
-  if (!map.size){
-    throw("Cannot process")
+  if (!map.size) {
+    throw (new Error('Cannot process'));
   }
-  for (const [key, value] of map){
-    if (value === 1){
+  for (const [key, value] of map) {
+    if (value === 1) {
       map.set(key, 100);
     }
   }
