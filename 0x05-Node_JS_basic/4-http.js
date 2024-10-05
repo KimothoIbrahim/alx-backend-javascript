fa02ae9c715aec051bbf51e444e85f230405ee20
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-10-05 15:42:34 +0300

    Make a hhtp server with Node

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,13 @@
 const http = require('http');
 
-const server = http.createServer(() => { console.log('Every request'); });
-server.listen(1245);
+const app = http.createServer();
+app.listen(1245);
 
 const body = 'Hello Holberton School!';
 
-server.on('request', (request, response) => {
+app.on('request', (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain', 'Content-Length': body.length });
-  return response.end(body, () => console.log('response sent'));
+  return response.end(body);
 });
+
+module.exports = app;
